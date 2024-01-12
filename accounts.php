f99dc4e29737c89d3d1fcb3dbb86d389eb626d9f
avelaa06 <maksym06h@gmail.com>
2024-01-12 09:43:36 +0000

    Finished transactions API and fixed accounts.php variable naming error.

--- accounts.php
+++ accounts.php
@@ -15,10 +15,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 $conn->close();
 
 function handleGetRequest($conn) {
-    global $transactions_table_name;
+    global $accounts_table_name;
     if (isset($_GET['id'])) {
         $accountId = $_GET['id'];
-        $stmt = $conn->prepare("SELECT * FROM $transactions_table_name WHERE id = ?");
+        $stmt = $conn->prepare("SELECT * FROM $accounts_table_name WHERE id = ?");
         $stmt->bind_param("i", $accountId);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -32,7 +32,7 @@ function handleGetRequest($conn) {
         $stmt->close();
     } elseif (isset($_GET['user_id'])) {
         $userId = $_GET['user_id'];
-        $stmt = $conn->prepare("SELECT * FROM $transactions_table_name WHERE user_id = ?");
+        $stmt = $conn->prepare("SELECT * FROM $accounts_table_name WHERE user_id = ?");
         $stmt->bind_param("i", $userId);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -43,7 +43,7 @@ function handleGetRequest($conn) {
         }
         sendJsonResponse(200, $accounts);
     } else {
-        $result = $conn->query("SELECT * FROM $transactions_table_name");
+        $result = $conn->query("SELECT * FROM $accounts_table_name");
         $accounts = [];
         while ($row = $result->fetch_assoc()) {
             $accounts[] = $row;
@@ -53,7 +53,7 @@ function handleGetRequest($conn) {
 }
 
 function handlePostRequest($conn) {
-    global $transactions_table_name;
+    global $accounts_table_name;
     $data = json_decode(file_get_contents("php://input"), true);
 
     $user_id = $data['user_id'];
@@ -61,7 +61,7 @@ function handlePostRequest($conn) {
     $currency = $data['currency'];
     $balance = $data['balance'];
 
-    $stmt = $conn->prepare("INSERT INTO $transactions_table_name (user_id, name, currency, balance) VALUES (?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO $accounts_table_name (user_id, name, currency, balance) VALUES (?, ?, ?, ?)");
     $stmt->bind_param("ssss", $user_id, $name, $currency, $balance);
     $stmt->execute();
 
@@ -74,7 +74,7 @@ function handlePostRequest($conn) {
 }
 
 function handlePutRequest($conn) {
-    global $transactions_table_name;
+    global $accounts_table_name;
     $data = json_decode(file_get_contents("php://input"), true);
 
     $id = $data['id'];
@@ -82,36 +82,37 @@ function handlePutRequest($conn) {
     $currency = $data['currency'];
     $balance = $data['balance'];
 
-    $stmt = $conn->prepare("SELECT * FROM $transactions_table_name WHERE id = ?");
+    $stmt = $conn->prepare("SELECT * FROM $accounts_table_name WHERE id = ?");
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if ($result !== false && $result->num_rows > 0) {
-        $stmt = $conn->prepare("UPDATE $transactions_table_name SET name = ?, currency = ?, balance = ? WHERE id = ?");
-        $stmt->bind_param("sssi", $name, $currency, $balance, $id);
-        $stmt->execute();
-
-        if ($stmt->affected_rows > 0) {
-            sendJsonResponse(200, ['message' => 'Account updated successfully']);
-        } else {
-            sendJsonResponse(400, ['error' => 'Query execution failed: ' . $conn->error]);
-        }
-        $stmt->close();
-    } else {
-        sendJsonResponse(404, ['error' => 'Account not found']);
+    if ($result === false || $result->num_rows === 0) {
+        sendJsonResponse(404, ["error"=> "Account not found"]);
+        return;
     }
+
+    $stmt = $conn->prepare("UPDATE $accounts_table_name SET name = ?, currency = ?, balance = ? WHERE id = ?");
+    $stmt->bind_param("sssi", $name, $currency, $balance, $id);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        sendJsonResponse(200, ['message' => 'Account updated successfully']);
+    } else {
+        sendJsonResponse(400, ['error' => 'Query execution failed: ' . $conn->error]);
+    }
+    $stmt->close();
 }
 
 function handleDeleteRequest($conn) {
-    global $transactions_table_name;
+    global $accounts_table_name;
     global $users_table_name;
     $data = json_decode(file_get_contents("php://input"), true);
 
     if (isset($data['id'])) {
         $id = $data['id'];
     
-        $stmt = $conn->prepare("DELETE FROM $transactions_table_name WHERE id = ?");
+        $stmt = $conn->prepare("DELETE FROM $accounts_table_name WHERE id = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
     
@@ -126,20 +127,20 @@ function handleDeleteRequest($conn) {
         $stmt->bind_param("i", $user_id);
         $stmt->execute();
         $result = $stmt->get_result();
-        if ($result !== false && $result->num_rows > 0) {
-            $stmt = $conn->prepare("DELETE FROM $transactions_table_name WHERE user_id = ?");
-            $stmt->bind_param("i", $user_id);
-            $stmt->execute();
-            if ($stmt->affected_rows > 0) {
-                sendJsonResponse(200, ["message" => "Accounts deleted successfully"]);
-            } else {
-                sendJsonResponse(200, ["message" => "User has no accounts"]);
-            }
+        if ($result === false || $result->num_rows === 0) {
+            sendJsonResponse(404, ["error"=> "User not found"]);
+            return;
+        }
+        $stmt = $conn->prepare("DELETE FROM $accounts_table_name WHERE user_id = ?");
+        $stmt->bind_param("i", $user_id);
+        $stmt->execute();
+        if ($stmt->affected_rows > 0) {
+            sendJsonResponse(200, ["message" => "Accounts deleted successfully"]);
         } else {
-            sendJsonResponse(404, ["error" => "User doesn't exist"]);
+            sendJsonResponse(200, ["message" => "User has no accounts"]);
         }
     } else {
-        $stmt = $conn->prepare("DELETE FROM $transactions_table_name");
+        $stmt = $conn->prepare("DELETE FROM $accounts_table_name");
         $stmt->execute();
     
         if ($stmt->affected_rows > 0) {
--- transactions.php
+++ transactions.php
@@ -65,9 +65,12 @@ function handleGetRequest($conn) {
             return;
         }
 
-        $stmt = $conn->prepare("SELECT $transactions_table_name.id, $transactions_table_name.amount FROM $transactions_table_name INNER JOIN $accounts_table_name ON 
-            $transactions_table_name.account_id = $accounts_table_name.id WHERE user_id = ? 
-            GROUP BY $transactions_table_name.id");
+        $t = $transactions_table_name;
+        $stmt = $conn->prepare(
+            "SELECT $t.id, $t.account_id, $t.category_id, $t.amount, $t.is_income, $t.payment_date, $t.description 
+            FROM $t INNER JOIN $accounts_table_name ON 
+            $t.account_id = $accounts_table_name.id WHERE user_id = ? 
+            GROUP BY $t.id");
         $stmt->bind_param("i", $userId);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -91,17 +94,25 @@ function handlePostRequest($conn) {
     global $transactions_table_name;
     $data = json_decode(file_get_contents("php://input"), true);
 
-    $user_id = $data['user_id'];
-    $name = $data['name'];
-    $currency = $data['currency'];
-    $balance = $data['balance'];
+    $accountId = $data["account_id"];
+    $categoryId = $data["category_id"];
+    $amount = $data["amount"];
+    $isIncome = (int)$data["is_income"];
+    $paymentDate = $data["payment_date"];
+    $description = $data["description"];
+    if ($description == "") {
+        $description = null;
+    }
 
-    $stmt = $conn->prepare("INSERT INTO $transactions_table_name (user_id, name, currency, balance) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("ssss", $user_id, $name, $currency, $balance);
+    $stmt = $conn->prepare(
+        "INSERT INTO $transactions_table_name 
+        (account_id, category_id, amount, is_income, payment_date, description)
+        VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssss", $accountId, $categoryId, $amount, $isIncome, $paymentDate, $description);
     $stmt->execute();
 
     if ($stmt->affected_rows > 0) {
-        sendJsonResponse(201, ['message' => 'Account added successfully']);
+        sendJsonResponse(201, ['message' => 'Transaction added successfully']);
     } else {
         sendJsonResponse(400, ['error' => 'Query execution failed: ' . $conn->error]);
     }
@@ -112,35 +123,45 @@ function handlePutRequest($conn) {
     global $transactions_table_name;
     $data = json_decode(file_get_contents("php://input"), true);
 
-    $id = $data['id'];
-    $name = $data['name'];
-    $currency = $data['currency'];
-    $balance = $data['balance'];
+    $id = $data["id"];
+    $accountId = $data["account_id"];
+    $categoryId = $data["category_id"];
+    $amount = $data["amount"];
+    $isIncome = (int)$data["is_income"];
+    $paymentDate = $data["payment_date"];
+    $description = $data["description"];
+    if ($description == "") {
+        $description = null;
+    }
 
     $stmt = $conn->prepare("SELECT * FROM $transactions_table_name WHERE id = ?");
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    if ($result !== false && $result->num_rows > 0) {
-        $stmt = $conn->prepare("UPDATE $transactions_table_name SET name = ?, currency = ?, balance = ? WHERE id = ?");
-        $stmt->bind_param("sssi", $name, $currency, $balance, $id);
-        $stmt->execute();
-
-        if ($stmt->affected_rows > 0) {
-            sendJsonResponse(200, ['message' => 'Account updated successfully']);
-        } else {
-            sendJsonResponse(400, ['error' => 'Query execution failed: ' . $conn->error]);
-        }
-        $stmt->close();
-    } else {
-        sendJsonResponse(404, ['error' => 'Account not found']);
+    if ($result === false || $result->num_rows === 0) {
+        sendJsonResponse(404, ['error' => 'Transaction not found']);
+        return;
     }
+
+    $stmt = $conn->prepare(
+        "UPDATE $transactions_table_name 
+        SET account_id = ?, category_id = ?, amount = ?, is_income = ?, payment_date = ?, description = ?
+        WHERE id = ?");
+    $stmt->bind_param("ssssssi", $accountId, $categoryId, $amount, $isIncome, $paymentDate, $description, $id);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        sendJsonResponse(200, ['message' => 'Transaction updated successfully']);
+    } else {
+        sendJsonResponse(400, ['error' => 'Query execution failed: ' . $conn->error]);
+    }
+    $stmt->close();
 }
 
 function handleDeleteRequest($conn) {
     global $transactions_table_name;
-    global $users_table_name;
+    global $accounts_table_name;
     $data = json_decode(file_get_contents("php://input"), true);
 
     if (isset($data['id'])) {
@@ -151,36 +172,39 @@ function handleDeleteRequest($conn) {
         $stmt->execute();
     
         if ($stmt->affected_rows > 0) {
-            sendJsonResponse(200, ["message" => "Account deleted successfully"]);
+            sendJsonResponse(200, ["message" => "Transaction deleted successfully"]);
         } else {
-            sendJsonResponse(404, ["error" => "Account not found"]);
+            sendJsonResponse(404, ["error" => "Transaction not found"]);
         }
-    } elseif (isset($data['user_id'])){
-        $user_id = $data['user_id'];
-        $stmt = $conn->prepare("SELECT * FROM $users_table_name WHERE id = ?");
-        $stmt->bind_param("i", $user_id);
+    } elseif (isset($data['account_id'])) {
+        $accountId = $data['account_id'];
+
+        $stmt = $conn->prepare("SELECT * FROM $accounts_table_name WHERE id = ?");
+        $stmt->bind_param("i", $accountId);
         $stmt->execute();
         $result = $stmt->get_result();
-        if ($result !== false && $result->num_rows > 0) {
-            $stmt = $conn->prepare("DELETE FROM $transactions_table_name WHERE user_id = ?");
-            $stmt->bind_param("i", $user_id);
-            $stmt->execute();
-            if ($stmt->affected_rows > 0) {
-                sendJsonResponse(200, ["message" => "Accounts deleted successfully"]);
-            } else {
-                sendJsonResponse(200, ["message" => "User has no accounts"]);
-            }
+        if ($result === false || $result->num_rows === 0) {
+            sendJsonResponse(404, ["error" => "Account not found"]);
+            return;
+        }
+
+        $stmt = $conn->prepare("DELETE FROM $transactions_table_name WHERE account_id = ?");
+        $stmt->bind_param("i", $accountId);
+        $stmt->execute();
+
+        if ($stmt->affected_rows > 0) {
+            sendJsonResponse(200, ["message"=> "Transactions deleted successfully"]);
         } else {
-            sendJsonResponse(404, ["error" => "User doesn't exist"]);
+            sendJsonResponse(200, ["message"=> "Account has no transactions"]);
         }
     } else {
         $stmt = $conn->prepare("DELETE FROM $transactions_table_name");
         $stmt->execute();
     
         if ($stmt->affected_rows > 0) {
-            sendJsonResponse(200, ["message" => "Deleted all accounts"]);
+            sendJsonResponse(200, ["message" => "Deleted all transactions"]);
         } else {
-            sendJsonResponse(200, ["message" => "No accounts were found"]);
+            sendJsonResponse(200, ["message" => "No transactions were found"]);
         }
     }
 }
