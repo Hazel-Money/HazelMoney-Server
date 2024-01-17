d46aba33a00e04a0631c297601c83710acfdf371
avelaa06 <maksym06h@gmail.com>
2024-01-17 12:02:53 +0000

    Completed JWT generation

--- login.php
+++ login.php
@@ -1,8 +1,4 @@
 <?php
-include_once './config/database.php';
-require "../vendor/autoload.php";
-use \Firebase\JWT\JWT;
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
@@ -10,6 +6,9 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Max-Age: 3600");
 
 require_once 'db_connection.php';
+
+require "vendor/autoload.php";
+use \Firebase\JWT\JWT;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     handlePostRequest($conn);
@@ -29,24 +28,50 @@ function handlePostRequest($conn) {
 
     $stmt = $conn->prepare(
         "SELECT * FROM $users_table_name
-        WHERE email LIKE ?"
+        WHERE email = ?"
     );
     $stmt->bind_param("s", $email);
     $stmt->execute();
     $result = $stmt->get_result();
     $user = $result->fetch_assoc();
+    $password_hash = $user['password_hash'];
 
     if (!$user) {
         sendJsonResponse(400, ["error"=> "Email or password does not match"]);
         return;
     }
-    if (!password_verify($password, $user['password_hash'])) {
+    if (!password_verify($password, $password_hash)) {
         sendJsonResponse(400, ['error'=> "Email or password does not match"]);
         return;
     }
 
-    sendJsonResponse(200, phpversion());
+    $id = $user["id"];
+    $username = $user["username"];
 
+    $secret_key = json_decode(file_get_contents("config/secret_key.json"), true)["secret_key"];
+    $issuer_claim = "hazelmoneydb";
+    $audience_claim = "localhost:80/api/";
+    $issuedate_claim = time(); // issued at
+    $notbefore_claim = $issuedate_claim; //not before in seconds
+    $expire_claim = $issuedate_claim + 60; // expire time in seconds
+    $token = array(
+        "iss" => $issuer_claim,
+        "aud" => $audience_claim,
+        "iat" => $issuedate_claim,
+        "nbf" => $notbefore_claim,
+        "exp" => $expire_claim,
+        "data" => array(
+            "id" => $id,
+            "email" => $email,
+            "username" => $username
+        ));
+    $jwt = JWT::encode($token, $secret_key, 'HS256');
+    sendJsonResponse(200, [
+        "message" => "Successful login",
+        "jwt" => $jwt,
+        "email" => $email,
+        "expireAt" => $expire_claim
+    ]);
 }
 
 function handleOptionsRequest($conn) {
