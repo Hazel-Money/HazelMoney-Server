6c9146b32adf96a550cbf55c1dde9c7a3c97cbdd
avelaa06 <maksym06h@gmail.com>
2024-01-19 16:05:40 +0000

    check_for_payments version 2

--- check_for_payments.php
+++ check_for_payments.php
@@ -3,11 +3,16 @@ require_once 'db_connection.php';
 
 global $regular_payments_table_name;
 global $frequencies_table_name;
+
 $result = $conn->query("SELECT * FROM $regular_payments_table_name WHERE id = 2");
-$regular_payment = $result->fetch_assoc();
-$result = $conn->query("SELECT sql_interval FROM $frequencies_table_name WHERE id = $regular_payment[frequency_id]");
-$frequency = $result->fetch_assoc();
+$row = $result->fetch_assoc();
+$last_payment_date = $row['last_payment_date'];
+
+$result = $conn->query("SELECT sql_interval FROM $frequencies_table_name WHERE id = $row[frequency_id]");
+$row = $result->fetch_assoc();
+$frequency = $row['sql_interval'];
+
 // $result = $conn->query("SELECT TIMESTAMPDIFF($frequency[sql_interval], $regular_payment[last_payment_date], NOW())");
-$result = $conn->query("SELECT TIMESTAMPDIFF(WEEK, '2022-01-01', NOW())");
+$result = $conn->query("SELECT TIMESTAMPDIFF($frequency, '$last_payment_date', NOW())");
 $time_difference = $result->fetch_row();
 echo json_encode($time_difference);
